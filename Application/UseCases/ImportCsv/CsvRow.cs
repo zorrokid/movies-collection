4cce8daf8d0712724d7beb51cb9dbfa755cd2694
Mikko Keinänen <mikko.keinanen@gmail.com>
2021-02-27 19:48:48 +0200

    empty values conversion

--- Application/UseCases/ImportCsv/CsvRow.cs
+++ Application/UseCases/ImportCsv/CsvRow.cs
@@ -5,17 +5,17 @@ namespace Application.UseCases.ImportCsv
         public bool IsChecked { get; set;}
         public string OriginalTitle { get; set;}
         public string LocalTitle { get; set;}
-        public int Year { get; set;}
+        public int? Year { get; set;}
         public string MediaType { get; set;}
         public string Type { get; set;}
         public string Edition { get; set; }
         public string Country { get; set; }
         public string CaseType { get; set; }
-        public int Discs { get; set; }
+        public int? Discs { get; set; }
         public bool HasSubFi { get; set; }
         public bool HasSubEn { get; set; }
         public string AspectRatio { get; set; }
-        public int RunningTime { get; set; }
+        public int? RunningTime { get; set; }
         public string Director { get; set; }
         public string Status { get; set; }
         public string Condition { get; set; }
@@ -24,10 +24,10 @@ namespace Application.UseCases.ImportCsv
         public bool IsRental { get; set; }
         public bool HasSlipCover { get; set; }
         public bool HasHologram { get; set; }
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Barcode { get; set; }
         public string IMDBCode { get; set; }
-        public int CollectionId { get; set; }	
+        public int? CollectionId { get; set; }	
         public string Series { get; set; }
         public string Publisher { get; set; }	
         public string Studio { get; set; }	
--- Application/UseCases/ImportCsv/YesNoBooleanConverter.cs
+++ Application/UseCases/ImportCsv/YesNoBooleanConverter.cs
@@ -9,7 +9,7 @@ namespace Application.UseCases.ImportCsv
         public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
         {
             string txtToBool = text.Trim().ToLower();
-            if (txtToBool != "yes" && txtToBool != "no") throw new CvsImportException("Values 'yes' or 'no' accepted for type conversion to boolean.");
+            if (txtToBool != "yes" && txtToBool != "no" && !string.IsNullOrEmpty(txtToBool)) throw new CvsImportException("Values 'yes', 'no' or empty (interpreted as 'no') accepted for type conversion to boolean.");
             return txtToBool == "yes";
         }
 
