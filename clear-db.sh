148e3f71d98a928db21a3a0d14cc9df65b18cc7b
Mikko Keinänen <mikko.keinanen@gmail.com>
2021-05-16 01:19:05 +0300

    help scripts for running migrations

---
+++ clear-db.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+dotnet ef database update 0 --startup-project ./MovieAPI/ --project ./Infrastructure/
---
+++ migrate-db.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+dotnet ef database update --startup-project ./MovieAPI/ --project ./Infrastructure/
