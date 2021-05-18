3b9763b4b522eceb9906ae7b1ad8977a6e6498a4
Mikko Keinänen <mikko.keinanen@gmail.com>
2021-05-18 23:35:34 +0300

    cleanup

--- MovieAPI/Startup.cs
+++ MovieAPI/Startup.cs
@@ -7,10 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
-using MovieAPI.Configure;
-using System;
 using System.Reflection;
 
 namespace movieAPI
