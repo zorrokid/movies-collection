94d557ca220631301e396f746a6d360322063217
Mikko Keinänen <mikko.keinanen@gmail.com>
2021-05-19 21:28:47 +0300

    cleanup and refactor

--- Application/Interfaces/INamedEntityRepository.cs
+++ Application/Interfaces/INamedEntityRepository.cs
@@ -1,15 +1,8 @@
-using System.Collections.Generic;
 using Domain.Entities;
 
 namespace Application.Interfaces
 {
-    public interface INamedEntityRepository<T> where T : NameEntity
-    {
-        List<T> GetByName(string name);
-        IEnumerable<T> GetAll();
-        T GetById(int id);
-        T Add(T entity);
-        T Update(T entity);
-        void Delete(int id);
-    }
+    public interface INamedEntityRepository<T> : IRepository<T> 
+        where T : NameEntity
+    {}
 }
--- Infrastructure/Persistence/Database/Repositories/NamedEntityRepository.cs
+++ Infrastructure/Persistence/Database/Repositories/NamedEntityRepository.cs
@@ -5,7 +5,8 @@ using Domain.Entities;
 
 namespace Infrastructure.Persistence.Database.Repositories
 {
-    public class NamedEntityRepository<T> : GenericRepository<T>, IRepository<T>, INamedEntityRepository<T> where T : NameEntity
+    public class NamedEntityRepository<T> : GenericRepository<T>, IRepository<T>, INamedEntityRepository<T> 
+        where T : NameEntity
     {
         public NamedEntityRepository(MoviesContext context) : base(context)
         {
