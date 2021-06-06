5b2a877ae80bb434f7331d2311333edf7dd9c690
Mikko Keinänen <mikko.keinanen@gmail.com>
2021-06-06 19:41:59 +0300

    Rename Username => UserName

--- Application/UseCases/AuthenticateUseCase/AuthenticateResponse.cs
+++ Application/UseCases/AuthenticateUseCase/AuthenticateResponse.cs
@@ -5,7 +5,7 @@ namespace Application.UseCases.AuthenticateUseCase
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Username { get; set; }
+        public string UserName { get; set; }
         public string Token { get; set; }       
     }
 }
