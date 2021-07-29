0e50edfb4104b6496995cf66b865fe7138b54ad3
Mikko Keinänen <mikko.keinanen@gmail.com>
2021-07-29 15:25:20 +0300

    Migration for publication country code

--- Infrastructure/Migrations/Application/20210729122351_PublicationCountryCodes.Designer.cs
+++ Infrastructure/Migrations/Application/20210729122351_PublicationCountryCodes.Designer.cs
@@ -2,15 +2,17 @@
 using Infrastructure.Persistence.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Infrastructure.Migrations.Application
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20210729122351_PublicationCountryCodes")]
+    partial class PublicationCountryCodes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Infrastructure/Migrations/Application/20210729122351_PublicationCountryCodes.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Infrastructure.Migrations.Application
+{
+    public partial class PublicationCountryCodes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CountryCode",
+                table: "Publications");
+
+            migrationBuilder.CreateTable(
+                name: "PublicationCountryCode",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PublicationId = table.Column<int>(type: "integer", nullable: false),
+                    CountryCode = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PublicationCountryCode", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PublicationCountryCode_Publications_PublicationId",
+                        column: x => x.PublicationId,
+                        principalTable: "Publications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PublicationCountryCode_PublicationId",
+                table: "PublicationCountryCode",
+                column: "PublicationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PublicationCountryCode");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CountryCode",
+                table: "Publications",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
